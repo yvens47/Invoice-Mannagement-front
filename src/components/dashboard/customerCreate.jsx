d6904dad092e7d51b7b9bd79c68b8759680196d4
Jean <jyvenspierre@gmail.com>
2022-05-30 17:37:38 -0400

    attach events

--- src/components/dashboard/customerCreate.jsx
+++ src/components/dashboard/customerCreate.jsx
@@ -5,77 +5,72 @@ import Box from '@mui/material/Box'
 import TextField from '@mui/material/TextField'
 import Button from "@mui/material/Button"
 import states from "../../utils/states_hash.json";
+import Select from '@mui/material/Select';
 
 
 
 import MenuItem from '@mui/material/MenuItem';
 
-function CustomerCreate (props){
-  const [state, setState] = React.useState('EUR'); 
+function CustomerCreate(props) {
+  const [state, setState] = React.useState('EUR');
 
   const handleChange = (event) => {
     setState(event.target.value);
   };
 
-  const handleAdd = (e)=>{
+  const handleAdd = (e) => {
     e.preventDefault();
     // add company details
     alert("alert company added ");
-    
-    
+
+
   }
 
-  return(
-    <div className='border p-3' style={{width:'500px'}}>
+  return (
+    <div className='border p-3' style={{ width: '500px' }}>
 
       <div className='d-flex flex-column '>
         <InfoIcon />
-         <h1 className='display-5'>Enter Details</h1>
-        <Box className='' component='form' style={{width:'100%'}}>
+        <h1 className='display-5'>EMy Company Details</h1>
+        <Box className='' component='form' style={{ width: '100%' }}>
           <div className='mb-1'>
-             <TextField  fullWidth variant='outlined' label='Business Name' placeHolder='name here' />
+            <TextField  name='name'onChange={props.change} fullWidth variant='outlined' label='Business Name' placeHolder='name here' />
           </div>
           <div className='mb-1'>
-             <TextField fullWidth type='email' variant='outlined' label='Email' placeHolder='email here' />
+            <TextField name='email' onChange={props.change} fullWidth type='email' variant='outlined' label='Email' placeHolder='email here' />
           </div>
           <div className='mb-1 d-flex-column'>
             <div className='mb-2 d-flex-column'>
-              <TextField fullWidth variant='outlined' label='Street' placeHolder='name here' />
+              <TextField name='' onChange={props.change} fullWidth variant='outlined' label='Street' placeHolder='name here' />
             </div>
             <div className='mb-1 d-flex'>
-               <TextField className='me-1 flex-shrink-1'  variant='outlined' label='City' placeHolder='name here' />
-     
-               <TextField
-          id="outlined-select-currency"
-          select
-              className='me-1 flex-grow-1'
-          
-                 
-          label="State"
-          value={state}
-          onChange={handleChange}
-         
-        >
-          {states && Object.keys(states).map((option) => (
-            <MenuItem key={option} value={option}>
-              {option}
-            </MenuItem>
-          ))}
-        </TextField>
-              <TextField style={{width:"120px"}}   className='me-1 flex-shrink-1 ' variant='outlined' label='Zip Code' placeHolder='name here' />
-            </div>
-            
-          </div>
-          <div><Button size='large' variant='outlined' type='submit' onClick ={handleAdd}>Add</Button></div>
-         
-        </Box>
-      
-      </div>
-     
+              <TextField  name='city'onChange={props.change} className='me-1 flex-shrink-1' variant='outlined' label='City' placeHolder='name here' />
 
-      
-    
-    
+             
+                <Select name='state' label="State" onChange={props.change} className='me-1 flex-grow-1'>
+                {states && Object.keys(states).map((option) => (
+                  <MenuItem key={option} value={option}>
+                    {option}
+                  </MenuItem>
+                ))}
+
+                </Select>
+               
+             
+              <TextField name='zip' onChange={props.change} style={{ width: "120px" }} className='me-1 flex-shrink-1 ' variant='outlined' label='Zip Code' placeHolder='name here' />
+            </div>
+
+          </div>
+          <div><Button size='large' variant='outlined' type='submit' onClick={props.addCompany}>Add</Button></div>
+
+        </Box>
+
+      </div>
+
+
+
+
+
     </div>
   )
 }
--- src/components/dashboard/home.jsx
+++ src/components/dashboard/home.jsx
@@ -5,25 +5,40 @@ import DialogBox from './dialog';
 import CustomerCreate from './customerCreate'
 
 
-const company ={
+const company = {
 
 
-  inovices:[],
-  users:[],
-  name:String,
-  address:{},
-  balance:String,
-  email:String,
-  about:String
-  
+	inovices: [],
+	users: [],
+	name: String,
+	address: {},
+	balance: String,
+	email: String,
+	about: String
 
-  
+
+
 
 }
 
 function DashboardHome(props) {
- 
+
 	const [open, setOpen] = useState(false);
+	const [companyDetail, setCompanyDetail] = useState({
+
+
+
+
+		name: String,
+		address: {},
+		balance: String,
+		email: String,
+		about: String
+
+
+
+
+	})
 
 	const handleClickOpen = () => {
 		setOpen(true);
@@ -32,11 +47,36 @@ function DashboardHome(props) {
 	const handleClose = () => {
 		setOpen(false);
 	};
+
+	const addCompany = (e) => {
+		e.preventDefault();
+
+		console.log(companyDetail);
+	}
+	const handleChange = (e) => {
+		var state = '';
+		const companyDetailCopy = { ...companyDetail }
+		if (e.currentTarget === null) {
+			state = e.target.value;
+			companyDetailCopy.address.state = state
+
+		} else {
+			const { value, name } = e.target;
+			console.log(e.currentTarget)
+			// make copy of company details object
+
+			console.log(name, value)
+
+		}
+
+
+
+	}
 	return (
 		<div className="col-md-10 main-content">
 			<h1 className="payment display-3">Set Up payments</h1>
 			<p className="lead">
-			
+
 				Etiam varius maximus vulputate. In at arcu diam. Nulla id mauris et eros
 				dictum convallis id eu urna. Praesent sed convallis libero
 			</p>
@@ -45,19 +85,19 @@ function DashboardHome(props) {
 			<div className=" card-wrapper d-flex flex-row">
 				<div className="card me-3" style={{ width: '250px' }}>
 					<div className="card-body">
-						<h5 className="card-title">Set Your Company Details</h5>
+						<h5 className="card-title">Register you company</h5>
 						<p className="card-text">
 							Some quick example text to build on the card title and m.
 						</p>
-            <Button variant="outlined" onClick={handleClickOpen}>
-        Add Company Details
-      </Button>
-						
+						<Button variant="outlined" onClick={handleClickOpen}>
+							Add Company Details
+						</Button>
+
 					</div>
 				</div>
 
 				<div className="card" style={{ width: '250px' }}>
-          
+
 					<div className="card-body">
 						<h5 className="card-title">Connect Bank</h5>
 						<p className="card-text">
@@ -70,16 +110,16 @@ function DashboardHome(props) {
 				</div>
 			</div>
 			<DialogBox
-        content={
-        <CustomerCreate />
-        
-      }
+				content={
+					<CustomerCreate addCompany={addCompany} change={handleChange} />
+
+				}
 				open={open}
 				handleClickOpen={handleClickOpen}
 				handleClose={handleClose}
 			>
-       
-        </DialogBox>
+
+			</DialogBox>
 		</div>
 	);
 }
